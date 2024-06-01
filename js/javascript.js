bd4030d5ed76298078d990ec3a91b477dccf60da
NowhereMan3 <147972435+NowhereMan3@users.noreply.github.com>
2024-06-01 16:31:49 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 let map = L.map('map').setView([58.373523, 26.716045], 12)
 const osm =
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
- maxZoom: 19,
- attribution: 'OpenStreetMap contributors',
-})
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
 osm.addTo(map)
